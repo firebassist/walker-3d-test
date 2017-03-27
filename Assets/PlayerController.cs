0106e494ea55b0e13df0f030ff22f393513471a0
mgdadacon <mgdadacon@gmail.com>
2017-03-28 10:23:20 +1300

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float walkSpeed = 2;
+
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+
+
+	void Update () {
+
+		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+		Vector2 inputDir = input.normalized;
+
+		if (inputDir != Vector2.zero) {
+			transform.eulerAngles = Vector3.up * Mathf.Atan2 (inputDir.x, inputDir.y) * Mathf.Rad2Deg;	
+		}
+
+		float speed = walkSpeed * inputDir.magnitude;
+
+		transform.Translate (transform.forward * speed * Time.deltaTime, Space.World);
+
+		float animationSpeedPercent = 1 * inputDir.magnitude;
+		animator.SetFloat ("speedPercent", animationSpeedPercent);
+	}
+}
